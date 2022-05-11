01072bed8fa5db6f8f0c81f709103cf18c7e466a
Andrey Arsbkv <andrbeckov@gmail.com>
2022-05-11 09:41:58 +0300

    1_2 разбил рисование по функциям, поправил dnd (раньше таскался только за угол)

--- 1/2/index.ts
+++ 1/2/index.ts
@@ -1,39 +1,50 @@
 import {useDnd} from '../../src/dnd'
 
-// todo вынести из обработчика загрузки, разбить на функции
 document.addEventListener('DOMContentLoaded', () => {
+	drawPicture()
+})
+
+function drawPicture() {
 	const canvas = document.getElementsByTagName('canvas')[0]
 	canvas.width = 220
 	canvas.height = 140
 	useDnd(canvas)
 
 	const ctx = canvas.getContext('2d')
-
-	ctx.fillStyle = 'rgb(186,237,239)'
-	ctx.fillRect(0, 0, canvas.width, 110)
-
-	ctx.fillStyle = 'rgb(103,159,109)'
-	ctx.fillRect(0, 110, canvas.width, 30)
-
-	ctx.fillStyle = 'rgb(192,154,110)'
-	ctx.fillRect(60, 60, 100, 60)
-
-	ctx.fillStyle = 'rgb(168,133,93)'
-	ctx.fillRect(90, 120, 40, 3)
-
-	ctx.fillStyle = 'rgb(175,80,67)'
-	ctx.fillRect(140, 0, 20, 50)
-
-	const roof = new Path2D()
-	roof.moveTo(40, 60)
-	roof.lineTo(110, 10)
-	roof.lineTo(180, 60)
-	roof.lineTo(40, 60)
-	ctx.fillStyle = 'rgb(114,91,72)'
-	ctx.fill(roof)
-
+	drawSky(ctx, canvas)
+	drawGrass(ctx, canvas)
+	drawWalls(ctx)
+	drawPipe(ctx)
+	drawDoor(ctx)
+	drawRoof(ctx)
 	drawWindow(ctx, 65, 70)
 	drawWindow(ctx, 125, 70)
+	drawFence(ctx)
+}
+
+function drawSky(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+	ctx.fillStyle = 'rgb(186,237,239)'
+	ctx.fillRect(0, 0, canvas.width, 110)
+}
+
+function drawGrass(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+	ctx.fillStyle = 'rgb(103,159,109)'
+	ctx.fillRect(0, 110, canvas.width, 30)
+}
+
+function drawWalls(ctx: CanvasRenderingContext2D) {
+	ctx.fillStyle = 'rgb(192,154,110)'
+	ctx.fillRect(60, 60, 100, 60)
+}
+
+function drawPipe(ctx: CanvasRenderingContext2D) {
+	ctx.fillStyle = 'rgb(175,80,67)'
+	ctx.fillRect(140, 0, 20, 50)
+}
+
+function drawDoor(ctx: CanvasRenderingContext2D) {
+	ctx.fillStyle = 'rgb(168,133,93)'
+	ctx.fillRect(90, 120, 40, 3)
 
 	ctx.fillStyle = 'rgb(114,91,72)'
 	ctx.fillRect(100, 90, 20, 30)
@@ -42,12 +53,24 @@ document.addEventListener('DOMContentLoaded', () => {
 	ctx.arc(103, 108, 2, 0, Math.PI * 2, true)
 	ctx.fillStyle = 'rgb(161,127,100)'
 	ctx.fill()
+}
 
+function drawRoof(ctx: CanvasRenderingContext2D) {
+	const roof = new Path2D()
+	roof.moveTo(40, 60)
+	roof.lineTo(110, 10)
+	roof.lineTo(180, 60)
+	roof.lineTo(40, 60)
+	ctx.fillStyle = 'rgb(114,91,72)'
+	ctx.fill(roof)
+}
+
+function drawFence(ctx: CanvasRenderingContext2D) {
 	for (let i = 0; i < 5; ++i) {
 		drawPlank(ctx, i * 15, 90)
 		drawPlank(ctx, 150 + i * 15, 90)
 	}
-})
+}
 
 function drawPlank(ctx: CanvasRenderingContext2D, x: number, y: number) {
 	ctx.fillStyle = 'rgb(110,99,86)'
